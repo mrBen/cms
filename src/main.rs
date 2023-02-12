bdd0f1ea2bfd2eee4a1f5a851e96c56044b36dca
Benjamin Collet <benjamin.collet@protonmail.ch>
2023-02-12 20:36:56 +0100

    Add recursive search for videos files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+use walkdir::{Error, WalkDir};
+
+/// Organize your series and movies.
+#[derive(Parser)]
+struct Cli {
+    /// The folder to scan for videos
+    folder: PathBuf,
+}
+
+fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+
+    let mut videos = list_videos(&args.folder);
+    videos.sort();
+    for video in videos {
+        println!("{:?}", video.file_name().expect("no file name"));
+    }
+
+    Ok(())
+}
+
+fn list_videos(folder: &PathBuf) -> Vec<PathBuf> {
+    let mut videos = Vec::new();
+
+    for entry in WalkDir::new(folder).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() {
+            // TODO: use `mime_classifier`
+            if entry.path().extension().expect("no extension") == "mp4"
+                || entry.path().extension().expect("no extension") == "mkv"
+            {
+                videos.push(entry.into_path());
+            }
+        }
+    }
+
+    videos
 }
